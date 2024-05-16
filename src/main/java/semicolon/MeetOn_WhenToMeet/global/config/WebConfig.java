d21a81ef8db391e85d14dff5f04595c6490ba75b
ManchanTime <lych0918@gachon.ac.kr>
2024-05-16 11:33:44 +0900

    feat: webConfig domain front.oguogu.store

--- src/main/java/semicolon/MeetOn_WhenToMeet/global/config/WebConfig.java
+++ src/main/java/semicolon/MeetOn_WhenToMeet/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://172.16.212.76:3000", "http://front.oguogu.store:3000", "http://front.172.16.212.76.end:3000")
                 .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With, requestId, Correlation-Id")
                 .allowCredentials(true)
